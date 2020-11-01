002682ae7fea85d09614447bf05935aa34e4eea9
ZYGAL0 <efimov.se@phystech.edu>
2020-11-01 20:15:01 +0300

    1st attempt

---
+++ SOR/cmake-build-debug/CMakeFiles/SOR.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SOR.dir/main.cpp.o"
+  "SOR"
+  "SOR.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SOR.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
